f517a4684eaaa516cbcab8cf2e100b16c4318d25
Ollie Norris <ollie.norris@mmtdigital.co.uk>
2023-06-16 09:46:56 +0200

    initial commit

---
+++ pages/auth/[...nextauth].js
@@ -0,0 +1,4 @@
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
